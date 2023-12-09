b0bee79a6931cb2bec6857a94ced42efb7ef3002
dream-of-future <130543078+dream-of-future@users.noreply.github.com>
2023-12-09 18:59:31 +0800

    V2.0 大版本更新
    修复绝大部分bug，并优化交互系统

--- class_12_8/hm_3.py
+++ class_12_8/hm_3.py
@@ -1,4 +1,7 @@
 import info
+
+ls = [31,28,31,30,31,30,31,31,30,31,30,31]
+
 def is_runnian(year):
     if year % 4 == 0 and year % 100 != 0:
         return 1
@@ -9,7 +12,6 @@ def is_runnian(year):
 
 
 def how_many_day(month,day,runnian):
-    ls = [31,28,31,30,31,30,31,31,30,31,30,31]
     sum = 0
     for i in range(0,month-1):
         sum += ls[i]
@@ -24,7 +26,7 @@ def interesting_num(num):   # 判断出生在当年的第n天是否是x的平方
         print(f'{num}这个数字一般有趣')
 
 if __name__ == '__main__':
-    born = input()
+    born = input("请以形如19700101的格式输入您的生日:")
     try:
         test = int(born)
     except ValueError:
@@ -34,15 +36,19 @@ if __name__ == '__main__':
         year = int(born[0:4])
         month = int(born[4:6])
         day = int(born[6:])
-        # print(f'Y{year}M{month}D{day}')
+        print(f'Y{year} M{month} D{day}')
 
-        ret = is_runnian(year)
-        if ret == 1:
-            print('是闰年')
+        if (month > 12 or day > ls[month-1]) or (month < 1 or day < 1):
+            print("欢迎来到地球，天外来客\nWelcome to earth,our friend who are from space")
+            print("请使用标准的地球纪年\nPlease use the standard earth's calendar")
         else:
-            print("不是闰年")
+            ret = is_runnian(year)
+            if ret == 1:
+                print('是闰年')
+            else:
+                print("不是闰年")
 
-        ret = how_many_day(month, day, ret)
-        print(f"第{ret}天")
+            ret = how_many_day(month, day, ret)
+            print(f"第{ret}天")
 
-        interesting_num(ret)
+            interesting_num(ret)
